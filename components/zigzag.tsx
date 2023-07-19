9791ec22296a8039cb6f43c1ddeed8403197157d
Nikhil Bhojwani <107989387+nikhil-apprely@users.noreply.github.com>
2023-07-19 20:08:33 +0530

    "last commit"

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image'
 import FeatImage01 from '@/public/images/features-03-image-01.jpg'
 import FeatImage02 from '@/public/images/features-03-image-02.jpg'
 import FeatImage03 from '@/public/images/features-03-image-03.jpg'
+import FeatImage04 from '@/public/images/features-03-image-04.jpg'
+import FeatImage05 from '@/public/images/features-03-image-05.jpg'
+import FeatImage06 from '@/public/images/features-03-image-06.jpg'
+import FeatImage07 from '@/public/images/features-03-image-07.jpg'
+import FeatImage08 from '@/public/images/features-03-image-08.jpg'
+import FeatImage09 from '@/public/images/features-03-image-09.jpg'
 
 export default function Zigzag() {
   return (
@@ -48,7 +54,7 @@ export default function Zigzag() {
                   src={FeatImage02}
                   width={540}
                   height={405}
-                  alt="Features 01"
+                  alt="Features 02"
                 />
               </div>
             </div>
@@ -65,7 +71,112 @@ export default function Zigzag() {
                   src={FeatImage03}
                   width={540}
                   height={405}
-                  alt="Features 01"
+                  alt="Features 03"
+                />
+              </div>
+            </div>
+
+            {/* 4th item */}
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage04}
+                  width={540}
+                  height={405}
+                  alt="Features 04"
+                />
+              </div>
+            </div>
+
+         
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage05}
+                  width={540}
+                  height={405}
+                  alt="Features 05"
+                />
+              </div>
+            </div>
+
+           
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage06}
+                  width={540}
+                  height={405}
+                  alt="Features 06"
+                />
+              </div>
+            </div>
+
+
+          
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage07}
+                  width={540}
+                  height={405}
+                  alt="Features 07"
+                />
+              </div>
+            </div>
+
+
+            
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage08}
+                  width={540}
+                  height={405}
+                  alt="Features 08"
+                />
+              </div>
+            </div>
+
+
+            {/* 3rd item */}
+            <div className="flex justify-center items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage09}
+                  width={540}
+                  height={405}
+                  alt="Features 09"
                 />
               </div>
             </div>
